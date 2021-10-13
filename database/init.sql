ed92fe6bd60b7f6f66e7fd28330cf3e450889c2d
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2021-10-13 14:29:29 +0100

    we made an update the creat_db, relating to the example.env ,
    Co-authored-by: cemalokten <60609268+cemalokten@users.noreply.github.com>
    
    Co-authored-by: MJOW1999 <72146072+MJOW1999@users.noreply.github.com>

--- database/init.sql
+++ database/init.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-DROP TABLE IF EXISTS users, sessions, posts CASCADE;
+DROP TABLE IF EXISTS users, sessions CASCADE;
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
@@ -14,16 +14,9 @@ CREATE TABLE sessions (
    data JSON NOT NULL
 );
 
--- CREATE TABLE posts (
---     id SERIAL PRIMARY KEY,
---     user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
---     text_content text,
---     created_at timestamp
--- );
-
 INSERT INTO users (email, password, name) VALUES
 (
-  'test@gmail.com',
+  'someone@gmail.com',
   '$2a$10$vzgLAxSa1k293giKSbVWi.GgSGmb1JB/kD1qWIg.mrUlt7UwVDCWG',
   'Test Testington'
 );
@@ -33,9 +26,5 @@ INSERT INTO sessions (sid, data) VALUES
   'abc123',
   '{"test":"stuff"}'
 );
-
--- INSERT INTO posts ( user_id, text_content, created_at) VALUES
---   (1,  'What is your favorite colour?', (SELECT CURRENT_TIMESTAMP)),
---   (1,  'What is your favorite animal?', (SELECT CURRENT_TIMESTAMP));
 
 COMMIT;
