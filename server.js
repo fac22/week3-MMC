a47bdb254d30be6587c3a275602f2d1e53c0365a
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2021-10-14 11:17:55 +0100

    changed the logIn to login and signUp to signup,
    Co-authored-by: cemalokten <60609268+cemalokten@users.noreply.github.com>
    
    Co-authored-by: MJOW1999 <72146072+MJOW1999@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const dotenv = require('dotenv');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
 const server = express();
 
 // Requires
-const signup = require('./routes/signup.js');
-const login = require('./routes/login.js');
-const profile = require('./routes/profile.js');
+const signup = require("./routes/signup.js");
+const login = require("./routes/login.js");
+const profile = require("./routes/profile.js");
 
 server.use(express.urlencoded({ extended: false }));
-server.use(express.static('./public'));
+server.use(express.static("./public"));
 server.use(cookieParser(process.env.COOKIE_SECRET));
 dotenv.config();
 
-server.get('/profile', profile.get);
-server.post('/', login.post);
-server.get('/', login.get);
-server.get('/sign-up', signup.get);
-server.post('/sign-up', signup.post);
+server.get("/profile", profile.get);
+server.post("/", login.post);
+server.get("/", login.get);
+server.get("/sign-up", signup.get);
+server.post("/sign-up", signup.post);
 
 const PORT = process.env.PORT || 3003;
 
-process.on('unhandledRejection', (error) => {
+process.on("unhandledRejection", (error) => {
   console.error(error);
   process.exit(1);
 });
