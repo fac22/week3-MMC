c54c6954705b062c0da932c26e26f5e81c4f8034
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2021-10-14 16:31:23 +0100

    made the app folder added the server.js to it as required by heroku

---
+++ app/server.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+const server = express();
+
+// Requires
+
+const signup = require("./routes/signup.js");
+const login = require("./routes/login.js");
+const profile = require("./routes/profile.js");
+const logOut = require("./routes/logOut.js");
+
+server.use(express.urlencoded({ extended: false }));
+server.use(express.static("./public"));
+server.use(cookieParser(process.env.COOKIE_SECRET));
+dotenv.config();
+
+server.get("/profile", profile.get);
+server.post("/profile", profile.post);
+server.post("/", login.post);
+server.get("/", login.get);
+server.get("/sign-up", signup.get);
+server.post("/sign-up", signup.post);
+
+server.post("/delete", profile.deleteReview);
+
+server.post("/log-out", logOut.post);
+
+const PORT = process.env.PORT || 3003;
+
+process.on("unhandledRejection", (error) => {
+  console.error(error);
+  process.exit(1);
+});
+
+server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
